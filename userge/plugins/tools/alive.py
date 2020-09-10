0710185dec7202216aad181f28ac201695031038
Dilkash <55246648+sdilkash@users.noreply.github.com>
2020-09-11 01:05:46 +0530

    Update alive.py

--- userge/plugins/tools/alive.py
+++ userge/plugins/tools/alive.py
@@ -25,13 +25,13 @@ async def alive(message: Message):
 **⌚ Up Time** : `{userge.uptime}`
 **💥 Version** : `{get_version()}`
 
-**sudo**: `{_parse_arg(Config.SUDO_ENABLED)}`
-**anti-spam**: `{_parse_arg(Config.ANTISPAM_SENTRY)}`
-**dual-mode**: `{_parse_arg(RawClient.DUAL_MODE)}`"""
+• **sudo**: `{_parse_arg(Config.SUDO_ENABLED)}`
+• **anti-spam**: `{_parse_arg(Config.ANTISPAM_SENTRY)}`
+• **dual-mode**: `{_parse_arg(RawClient.DUAL_MODE)}`"""
     if Config.HEROKU_APP:
         output += f"\n• **dyno-saver**: `{_parse_arg(Config.RUN_DYNO_SAVER)}`"
     output += f"""
-**unofficial**: `{_parse_arg(Config.LOAD_UNOFFICIAL_PLUGINS)}`
+• **unofficial**: `{_parse_arg(Config.LOAD_UNOFFICIAL_PLUGINS)}`
 
    **__python__**: `{versions.__python_version__}`
    **__pyrogram__**: `{versions.__pyro_version__}`
