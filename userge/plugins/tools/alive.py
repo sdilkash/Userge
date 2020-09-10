2d3094d6d7692abc98734ba9a945b03419f2f1ea
Dilkash <55246648+sdilkash@users.noreply.github.com>
2020-09-10 22:07:43 +0530

    Update alive.py

--- userge/plugins/tools/alive.py
+++ userge/plugins/tools/alive.py
@@ -68,7 +68,7 @@ async def _send_alive(message: Message, text: str) -> None:
 async def _refresh_id():
     global LOGO_ID, LOGO_REF  # pylint: disable=global-statement
     try:
-        gif = (await userge.get_messages('-1001260319807', 549042)).animation
+        gif = (await userge.get_messages('1001260319807', 549042)).animation
     except ChannelInvalid:
         LOGO_ID = None
         LOGO_REF = None
